5fc726999492b2f5261b9885a1184132467e772c
kazamiyuurei <144996217+KazamiYuryou@users.noreply.github.com>
2026-04-06 23:15:27 +0800

    Import teleportStyle and apply CSS to main page
    Import teleportStyle function and apply it to render styles in the main page.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './index.css';
 
 // 2. 导入酒馆助手官方提供的样式穿透工具函数。
 import { teleportStyle } from '../../util/script'; 
-
+ 
 $(() => {
   // 3. 由于脚本运行在隐藏的 iframe 中，原本的样式不会影响外部的酒馆网页。
   // 调用 teleportStyle() 将我们打包好的 CSS 强行“复制”到酒馆主界面的 <head> 中，从而渲染你的正则 HTML！
